cfcb9267e8300ab8a8d05e2f742aafad2b1b2742
Fernando Soares <fernando_soares346@hotmail.com>
2023-04-18 21:58:37 +0100

    router changes

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<Router>
+			<Router basename="/futebowl">
 				<App />
 			</Router>
 		</Provider>
